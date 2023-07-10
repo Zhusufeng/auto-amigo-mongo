b4cb92bb6cca6f821575e6dce630b27dc49da7ed
Lisa Gee <zhusufeng@gmail.com>
2023-07-09 21:29:48 -0700

    Create User API

---
+++ pages/api/user.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../lib/dbConnect";
+import User from "../../models/user.model";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const user = await User.find({}); /* find all the data in our database */
+        res.status(200).json({ success: true, data: user });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const user = await User.create(
+          req.body
+        ); /* create a new model in the database */
+        res.status(201).json({ success: true, data: user });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
