5fa6481ded0e3cfaff4048fd359f58d7b28ffa7b
Lisa Gee <zhusufeng@gmail.com>
2023-07-10 15:51:49 -0700

    Add message to UserForm

--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { mutate } from "swr";
 import { Button, Form, Input } from 'antd';
 
 const UserForm = () => {
   const contentType = "application/json";
+  const [message, setMessage] = useState("");
 
   /* The POST method adds a new entry in the mongodb database. */
   const onFinish = async (values: {
@@ -26,49 +28,51 @@ const UserForm = () => {
       }
       mutate("/api/user");
     } catch (error) {
-      // TODO show notification
-      console.log('Did not add user', error);
+      setMessage("Failed to add user");
     }
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        label="First Name"
-        name="firstName"
-        rules={[{ required: true, message: 'Please input your first name!' }]}
+    <div>
+      <p>{message}</p>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        onFinish={onFinish}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="First Name"
+          name="firstName"
+          rules={[{ required: true, message: 'Please input your first name!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Last Name"
-        name="lastName"
-        rules={[{ required: true, message: 'Please input your last name!' }]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Last Name"
+          name="lastName"
+          rules={[{ required: true, message: 'Please input your last name!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your email!' }]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
