2bfe049091c079f493c1a1d3716e0ee786b74578
Lisa Gee <zhusufeng@gmail.com>
2023-07-17 17:30:05 -0700

    Call InfoModal from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import useSWR, { mutate } from "swr";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, Card, Layout, Space, Tooltip } from "antd";
 import Head from "next/head";
 import { User } from "../lib/types";
-import { MAX_USERS, MAX_GAS_ENTRIES } from "../lib/constants";
+import InfoModal from "../components/InfoModal";
 import UserFormModal from "../components/UserFormModal";
 import GasCard from "../components/GasCard";
 import UsersList from "../components/UsersList";
@@ -22,9 +22,13 @@ const INITIAL_USER = {
 
 const Home = () => {
   const [user, setUser] = useState<User>(INITIAL_USER);
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
-
   const { data: users, error: userError } = useSWR("/api/user", fetcher);
+
+  useEffect(() => {
+    setIsInfoModalOpen(true);
+  }, [])
 
   const userHandleClick = (user: User) => {
     setUser(user);
@@ -39,31 +43,16 @@ const Home = () => {
       <Layout style={{ minHeight: "100vh" }}>
         <Layout.Header style={{ color: "#ffffff", fontWeight: "bold" }}>Auto Amigo Mongo</Layout.Header>
         <Layout.Content style={{ display: "flex", justifyContent: "center", padding: "20px" }}>
+          <InfoModal 
+            isModalOpen={isInfoModalOpen} 
+            setModalStatus={setIsInfoModalOpen} 
+          />
           <UserFormModal 
             isModalOpen={isUserModalOpen} 
             setModalStatus={setIsUserModalOpen} 
           />
           
           <Space direction="vertical" size="middle" style={{ width: "70%" }}>
-            <Card>
-              <h1>Background</h1>
-              <p>
-                This is a mini version of Auto Amigo. 
-                I made this to brush up on MongoDB, use Next.js&apos; pages API (serverless), 
-                and deploy on Vercel. 
-                It mimics Auto Amigo in the way that there can be multiple users, and their gas usage
-                and spending can be saved to the web app.
-              </p>
-              <h1>Instructions</h1>
-              <p>
-                Since this is a mini version, only {MAX_USERS} users can be created.
-                Each user can only have up to {MAX_GAS_ENTRIES} gas log entries. 
-                Everyone can view the users&apos; gas logs.
-                If there are no users, create a user.
-                Created users will show under &quot;Users&quot;. 
-                Select a user to view their gas log or to add to their gas log.
-              </p>
-            </Card>
             <Card>
               <Space direction="vertical" size="middle"  style={{ display: "flex" }}>
                 <div style={{ display: "flex", justifyContent: "space-between" }}>
