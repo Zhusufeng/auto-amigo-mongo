1c8ebdd0f957b3fd36ee6f83ebdd516e3a877def
Lisa Gee <zhusufeng@gmail.com>
2023-07-18 09:39:15 -0700

    Create Header

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Layout, Button } from "antd";
+import InfoModal from "./InfoModal";
+
+const Header = () => {
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+
+  return (
+    <>
+      <InfoModal 
+        isModalOpen={isInfoModalOpen} 
+        setModalStatus={setIsInfoModalOpen} 
+      />
+      <Layout.Header style={{ color: "#ffffff", fontWeight: "bold" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            Auto Amigo Mongo
+          </div>
+          <div>
+            <Button onClick={() => setIsInfoModalOpen(true)}>Instructions</Button>
+          </div>
+        </div>
+      </Layout.Header>
+    </>
+  )
+};
+
+export default Header;
