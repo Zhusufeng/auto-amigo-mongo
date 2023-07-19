999506a4072923474f4ed0623aecc1886b3a0cb2
Lisa Gee <zhusufeng@gmail.com>
2023-07-19 16:04:01 -0700

    Add CSRF protection to page & routes

--- pages/api/gas/[userId].ts
+++ pages/api/gas/[userId].ts
@@ -1,10 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { csrf } from "../../../lib/csrf";
 import dbConnect from "../../../lib/dbConnect";
 import { MAX_GAS_ENTRIES } from "../../../lib/constants";
 import Gas from "../../../models/gas.model";
 import User from "../../../models/user.model";
 
-export default async function handler(
+async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -67,3 +68,5 @@ export default async function handler(
       break;
   }
 }
+
+export default csrf(handler);
